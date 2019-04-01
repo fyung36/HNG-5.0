38281c51e4176f72d8435841f4681608d3ab130b
Efiong <Fyung36@gmail.com>
2019-04-01 15:41:34 +0100

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+print("Hello World!")
+print ('what is your name?')
+myname = input()
+print('it is good to you {}'.format(myname))
+print('the length of your name is:')
+print(len(myname))
+      
+print("what is your age?")
+myage = input()
+print("you will be {} in a year".format(str(int(myage)+1)))
